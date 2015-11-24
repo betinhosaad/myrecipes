24c2f5b35f7ec51ae9e25679ce54f6008d300b82
Alberto Saad <betinhosaad@hotmail.com>
2015-11-24 03:22:46 +0000

    create chefs and recipes association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123060742) do
+ActiveRecord::Schema.define(version: 20151124020632) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20151123060742) do
   end
 
   create_table "recipes", force: :cascade do |t|
+    t.integer "chef_id"
   end
 
 end
